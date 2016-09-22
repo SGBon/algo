882898651dd2aa33d1e2b75ad27ea5dd3b7c914e
Santiago Bonada <santiago.bonada@uoit.net>
2016-09-22 18:55:17 -0400

    quicksort, plotting

--- ass1/Q2/onemilsorts.py
+++ ass1/Q2/onemilsorts.py
@@ -1,5 +1,6 @@
 #sorts 1,000,000 strings using mergesort, quicksort, and radix sort
 
+# mergesort functions
 def merge(list1, list2):
     i, j = 0, 0
     list3 = []
@@ -32,25 +33,57 @@ def mergesort(list):
 
     return list
 
+# quicksort functions
+
+def partition(L, p, r):
+    x = L[r]
+    i = p - 1
+    for j in range(p, r):
+        if L[j] <= x:
+            i += 1
+            L[i], L[j] = L[j], L[i]
+    L[i+1], L[r] = L[r], L[i+1]
+    return i
+
+def quicksort(L,p,r):
+    if p < r:
+        k = partition(L,p,r)
+        quicksort(L,p,k-1)
+        quicksort(L,k+1,r)
 
 from random import choice
-from string import letters
+from string import uppercase
 
 def genstring(length):
-    return "".join(choice(letters) for i in range(length))
+    return "".join(choice(uppercase) for i in range(length))
 
 import time
 
 if __name__ == "__main__":
     unsorted = []
-    for i in range(1000000):
+    for i in range(10000):
         unsorted.append(genstring(100))
     print "sorting"
+
     mergetimes = []
     for i in range(100):
+        extra = list(unsorted)
         elapsed = time.clock()
-        mergesort(unsorted)
-        elapsed = time.clock() - elapsed
-        print elapsed
-        mergetimes.append(elapsed)
-    print mergetimes
+        mergesort(extra)
+        mergetimes.append(time.clock() - elapsed)
+
+    quicktimes = []
+    for i in range(100):
+        extra = list(unsorted) # need whole new list so we don't lose original
+        elapsed = time.clock()
+        quicksort(extra,0,len(extra)-1)
+        quicktimes.append(time.clock() - elapsed)
+
+    import matplotlib.pyplot as plt
+    plt.plot(mergetimes,label="Mergesort")
+    plt.plot(quicktimes,label="Quicksort")
+    plt.plot(quicktimes,label="Quicksort")
+    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), mode="expand", borderaxespad=0.)
+    plt.ylabel("Elapsed Time in Seconds")
+    plt.xlabel("Run")
+    plt.show()
