dd672cd4d46ce243ab83267bb7a60ccacd3d70e9
Santiago Bonada <Santiago.Bonada@uoit.net>
2016-09-29 11:05:17 -0400

    Updated million string sort with radix sort

--- ass1/Q2/onemilsorts.py
+++ ass1/Q2/onemilsorts.py
@@ -43,13 +43,61 @@ def partition(L, p, r):
             i += 1
             L[i], L[j] = L[j], L[i]
     L[i+1], L[r] = L[r], L[i+1]
-    return i
+    return i+1
 
 def quicksort(L,p,r):
     if p < r:
         k = partition(L,p,r)
         quicksort(L,p,k-1)
         quicksort(L,k+1,r)
+
+# radixsort functions
+# convert data to bit string
+def radix2(data):
+    if type(data) is int: # convert integer to bit string
+        return "{0:b}".format(data)
+    elif type(data) is str: # convert a character string to bit string
+        ret = ''.join('{0:08b}'.format(ord(x), 'b') for x in data) # need full bytes
+        return ret
+
+# Get the i-th digit of binary string x
+def digit(num,i):
+    x = radix2(num)
+    if i >= len(x):
+        return 0
+    else:
+        d = x[-(i+1)]
+        return 0 if d == '0' else 1
+
+def empty_array(size, init=None):
+    return [init for i in range(size)]
+
+# sort A using binary digit i
+def COUNTING_SORT(A,i):
+    n = len(A)
+    k = 2
+    B = empty_array(size=n)
+    C = empty_array(size=k,init=0)
+
+    for a in A:
+        d = digit(a,i)
+        C[d] = C[d] + 1
+
+    for j in range(1,k):
+        C[j] = C[j] + C[j-1]
+
+    for a in reversed(A):
+        d = digit(a,i)
+        B[C[d]-1] = a
+        C[d] = C[d] - 1
+
+    return B
+
+def RADIX_SORT(A):
+    n = max(len(radix2(x)) for x in A)
+    for i in range(n):
+        A = COUNTING_SORT(A,i)
+    return A
 
 from random import choice
 from string import uppercase
@@ -67,22 +115,29 @@ if __name__ == "__main__":
 
     mergetimes = []
     for i in range(100):
-        extra = list(unsorted)
+        extra = list(unsorted) # need whole new list so we don't lose original
         elapsed = time.clock()
         mergesort(extra)
         mergetimes.append(time.clock() - elapsed)
 
     quicktimes = []
     for i in range(100):
-        extra = list(unsorted) # need whole new list so we don't lose original
+        extra = list(unsorted)
         elapsed = time.clock()
         quicksort(extra,0,len(extra)-1)
         quicktimes.append(time.clock() - elapsed)
 
+    radxtimes = []
+    for i in range(100):
+        extra = list(unsorted)
+        elapsed = time.clock()
+        radixsort(extra)
+        radixtimes.append(time.clock() - elapsed)
+
     import matplotlib.pyplot as plt
     plt.plot(mergetimes,label="Mergesort")
     plt.plot(quicktimes,label="Quicksort")
-    plt.plot(quicktimes,label="Quicksort")
+    plot.plot(radixtimes,label="Radixsort")
     plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), mode="expand", borderaxespad=0.)
     plt.ylabel("Elapsed Time in Seconds")
     plt.xlabel("Run")
--- ass1/Q3/genhunmil.py
+++ ass1/Q3/genhunmil.py
@@ -1,10 +1,10 @@
 # generate 100,000,000 strings and write to file strings.txt
 
 from random import choice
-from string import letters
+from string import uppercase
 
 def genstring(length):
-    return "".join(choice(letters) for i in range(length))
+    return "".join(choice(uppercase) for i in range(length))
 
 if __name__ == "__main__":
     f = open("strings.txt","w")
